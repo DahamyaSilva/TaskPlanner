637dfeedd13242a1b40fa137a6dde1f49570057b
DahamyaSilva <dahamyapsilva@gmail.com>
2025-08-22 11:07:33 +0530

    added general styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react"
+import './assets/style.css'
+import AllTasks from "./components/taskOrganization/viewAllTasks/viewAllTasks"
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className="main-container">
+      <AllTasks />
+
+    </div>
   )
 }
 
---
+++ src/components/taskOrganization/AllTasks/allTasks.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './allTasks.css'
+
+function AllTasks (){
+
+    return (
+
+        <div className="all-tasks">
+            
+        </div>
+
+
+    )
+
+
+}
+
+export default AllTasks
